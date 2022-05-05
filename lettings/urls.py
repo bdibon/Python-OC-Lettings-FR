e1ceb1e528d460f4cf740479416eabdc2dad3360
Boris Dibon <boris.dibon@gmail.com>
2022-05-05 15:22:46 +0200

    Add namespaces to lettings and profiles urls

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "lettings"
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:letting_id>/', views.letting, name='letting'),
