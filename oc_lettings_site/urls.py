a18af4eb970d44cc5ceb51a259e6708d6fc8bc07
Boris Dibon <boris.dibon@gmail.com>
2022-05-05 15:15:58 +0200

    Move profiles code to progiles app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path("lettings/", include("lettings.urls")),
+    path("profiles/", include("profiles.urls")),
     path('admin/', admin.site.urls),
 ]
