0ecc4fe7bb60a0a7e6b90da20e602be609d607d7
Boris Dibon <boris.dibon@gmail.com>
2022-05-07 12:14:18 +0200

    Add oc_lettings_site test suite

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,10 @@
-def test_dummy():
-    assert 1
+from pytest_django.asserts import assertContains, assertTemplateUsed
+from django.urls import reverse
+
+
+def test_homepage_view(client):
+    homepage_url = reverse("site:index")
+    response = client.get(homepage_url)
+
+    assertContains(response, "<title>%s</title>" % "Holiday Homes", status_code=200)
+    assertTemplateUsed(response, "index.html")
